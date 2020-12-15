ea24e7b4ed77b61627a0379f73aaeaa894383f09
czechmate48 <cnsefcik@gmail.com>
2020-12-14 21:42:46 -0500

    building server

--- server.py
+++ server.py
@@ -5,13 +5,13 @@ from netutility import NetUtility
 
 DEFAULT_IP_ADDRESS = '192.168.1.1'
 DEFAULT_PORT = 8083
-PORT_MAXIMUM = 65535
-DATA_HEADER_LENGTH = 64
-DATA_FORMAT = 'utf-8'
+MAXIMUM_PORT = 65535
+DATA_HEADER_SIZE = 64  #Total length in Kb that server will accept at a time
+DATA_ENCODING = 'utf-8'
 
-LISTENING_MESSAGE = "[LISTENING] Server is now listening on {ip}:{port}..."
+LISTENING_START_MESSAGE = "[LISTENING] Server is now listening on {ip}:{port}..."
+LISTENING_STOP_MESSAGE = "[NOT LISTENING] Server stopped listening on {ip}:{port}"
 LISTENING_ERROR_MESSAGE = "[ERROR] Unable to listen on socket {ip}:{port}"
-
 
 class Server:
 
@@ -20,7 +20,7 @@ class Server:
     8080 is not available, the class increments the port number by 1 until it finds an available port."""
 
     def __init__(self, ip=DEFAULT_IP_ADDRESS, port=DEFAULT_PORT, display_terminal_output=False, listening=True):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.listening = listening
         self.display_terminal_output = display_terminal_output
         self.ip_address = Server.assign_ip_address(ip)
@@ -43,10 +43,10 @@ class Server:
 
         """If no port number is specified, the port stays as the DEFAULT_PORT. However, if the default port
         is unavailable, the port number will increment by 1 until a suitable port is found 
-        (stopping at PORT_MAXIMUM)"""
+        (stopping at MAXIMUM_PORT)"""
 
         if port == DEFAULT_PORT:
-            while not NetUtility.check_port_available(ip, port) and port <= PORT_MAXIMUM:
+            while not NetUtility.check_port_available(ip, port) and port <= MAXIMUM_PORT:
                 port += 1
             return port
         elif not NetUtility.check_port_available(ip, port):
@@ -57,35 +57,52 @@ class Server:
         """Starts the server by allowing clients to connect. If a client connects, a new thread is created
         to handle message transfers between server and client"""
 
-        self.server_socket.bind((self.ip_address, self.port))
-        self.listening = True
-        self.server_socket.listen(5)
-        if self.display_terminal_output:
-            print(LISTENING_MESSAGE.format(ip=str(self.ip_address), port=str(self.port)))
+        try:
+            self.socket.bind((self.ip_address, self.port))
+            self.listening = True
+            self.socket.listen(5)
+            listening_started = LISTENING_START_MESSAGE.format(ip=str(self.ip_address), port=str(self.port))
+            self.print_to_terminal(listening_started)
+        except:
+            listening_error = LISTENING_ERROR_MESSAGE.format(ip=str(self.ip_address), port=str(self.port))
+            self.print_to_terminal(listening_error)
         while self.listening:
-            client_connection, client_address = self.server_socket.accept()
+            client_connection, client_address = self.socket.accept()
             print("received connection from %r" % str(client_address))
             thread = threading.Thread(target=self.handle_client, args=(client_connection, client_address))
             thread.start()
 
+    def print_to_terminal(self, message):
+
+        """Prints messages to the terminal, but only if the server has been flagged to display output"""
+
+        if self.display_terminal_output:
+            print(message)
+
     def stop(self):
+
+        """Stops the server from listening and closes the socket"""
+
         self.listening = False
-        self.server_socket.close()
+        self.socket.close()
+        listening_stopped = LISTENING_STOPPED_MESSAGE.format(ip=str(ip_address), port=str(self.port))
+        self.print_to_terminal(listening_stopped)
 
     def handle_client(self, client_connection, client_address):
         client_connected = True
         connection_message = 'connected to server...' + "\r\n"
-        client_connection.send(connection_message.encode(DATA_FORMAT))
+        client_connection.send(connection_message.encode(DATA_ENCODING))
         while client_connected:
-            #  Identifies how long the message will be with a maximum size of DATA_HEADER_LENGTH
-            data_length = client_connection.recv(DATA_HEADER_LENGTH).decode(DATA_FORMAT) 
+            #  Identifies how long the message will be with a maximum size of DATA_HEADER_SIZE
+            data_length = client_connection.recv(DATA_HEADER_SIZE).decode(DATA_ENCODING) 
             #  Initial message upon connection is of length 0, only messages after initial should have header
             if data_length:
                 data_length = int(data_length)
-                data = client_connection.recv(data_length).decode(DATA_FORMAT)
+                data = client_connection.recv(data_length).decode(DATA_ENCODING)
                 print(data)
         client_connection.close()
 
 
 server = Server(display_terminal_output=True, listening=True)
 server.start()
+server.stop()
